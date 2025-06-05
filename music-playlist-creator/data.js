62a74cac84052d71a9cb5ae617d65c0f8f40ff46
Noah Pyrzanowski <noahpyrzanowski@meta.com>
2025-06-05 16:00:34 -0700

    Finishing up Milestone 4: Need to add displaying songs from data.js dynamically in the modal

---
+++ music-playlist-creator/data.js
@@ -0,0 +1,41 @@
+let reviews =  [
+        {
+        "playlist_image": "https://picsum.photos/200/300",
+        "playlist_name": "Main Playlist",
+        "playlist_author": "Peter Pipe", 
+        "playlist_likes": "3", 
+        "songs": [
+            {
+            "title": "a",
+            "artist": "",
+            "duration": ""
+            }
+        ]
+        },
+        {
+        "playlist_image": "https://picsum.photos/200/300",
+        "playlist_name": "Mike Bike Music",
+        "playlist_author": "Mike", 
+        "playlist_likes": "1",
+        "songs": [
+            {
+            "title": "b",
+            "artist": "",
+            "duration": ""
+            }
+        ]
+        },
+        {
+        "playlist_image": "https://picsum.photos/200/300",
+        "playlist_name": "Happy Playlist",
+        "playlist_author": "Mr. Happy", 
+        "playlist_likes": "1",
+        "songs": [
+            {
+            "title": "c",
+            "artist": "",
+            "duration": ""
+            }
+        ]
+        }
+    ]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,26 +1,82 @@
-// JavaScript for Opening and Closing the Modal
-const modal = document.getElementById("modal-overlay");
-const span = document.getElementsByClassName("close")[0];
+function createReviewElement(review) {
+   const reviewDiv = document.createElement('div');
+   reviewDiv.setAttribute('class', 'playlist-item')
 
-function openModal(playlist) {
-   document.getElementById('playlistImage').src = playlist.imageUrl;
-   document.getElementById('playlistTitle').innerText = playlist.name;
-   document.getElementById('artistName').innerText = `Dates: ${playlist.dates}`;
-   document.getElementById('playlistSongs').innerHTML = `${playlist.lineup.join(', ')}`;
+   const playlistImg = document.createElement("img");
+   playlistImg.setAttribute("class", "playlist-image");
+   playlistImg.src = review.playlist_image;
+   reviewDiv.appendChild(playlistImg);
+
+   const playlistName = document.createElement("h1");
+   playlistName.textContent = review.playlist_name;
+   reviewDiv.appendChild(playlistName);
+
+   const playlistAuthor = document.createElement("p");
+   playlistAuthor.textContent = review.playlist_author;
+   reviewDiv.appendChild(playlistAuthor);
+
+   const playlistLikes = document.createElement("p");
+   playlistLikes.textContent = review.playlist_likes;
+   reviewDiv.appendChild(playlistLikes);
+
+
+   reviewDiv.addEventListener('click', function() {
+   const modal = document.getElementById("modal-overlay");
+   const span = document.getElementsByClassName("close")[0];
+   const headerModal = document.getElementById('playlistTitle');
+   headerModal.innerHTML = review.playlist_name;
+   const authorModal = document.getElementById('artistName');
+   authorModal.innerHTML = review.playlist_author;
+   const imgModal = document.getElementById('playlistImage');
+   imgModal.src = review.playlist_image;
+   
    modal.style.display = "block";
+   span.onclick = function() {
+   modal.style.display = "none";
+   }
+   window.onclick = function(event) {
+      if (event.target == modal) {
+         modal.style.display = "none";
+      }
+   }
+   })
+   return reviewDiv;
 }
 
-span.onclick = function() {
-   modal.style.display = "none";
-}
-window.onclick = function(event) {
-   if (event.target == modal) {
-      modal.style.display = "none";
+function loadReviews() {
+   const reviewList = document.getElementById('playlist-cards')
+   
+   for (const review of reviews) {
+      const reviewElement = createReviewElement(review)
+      reviewList.appendChild(reviewElement)
    }
 }
 
+function errorPlaylistNotify() {
+   const reviewDiv = document.createElement('div');
+   reviewDiv.setAttribute('class', 'playlist-item');
+   const playlistName = document.createElement("h1");
+   playlistName.textContent = `Error: There are no playlists added!`;
+   reviewDiv.appendChild(playlistName);
 
+   return reviewDiv;
+}
 
-document.addEventListener("DOMContentLoaded",() => {
+function loaderrorPlaylists() {
+   const reviewList = document.getElementById('playlist-cards')
    
-})
+   for (const review of reviews) {
+      const reviewElement = errorPlaylistNotify(review)
+      reviewList.appendChild(reviewElement)
+   }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+   try {
+      loadReviews();
+   }
+   catch(error) {
+      loaderrorPlaylists();
+   }
+   // need to add support for whne the content of data.js for playlists is empty
+});
