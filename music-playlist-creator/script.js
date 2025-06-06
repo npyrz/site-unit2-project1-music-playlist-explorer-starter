4693835a12b65ff454418b8f9b26ccd40fdc49ff
Noah Pyrzanowski <noahpyrzanowski@meta.com>
2025-06-06 10:21:30 -0700

    Fixed issue with multiple songs for playlist not displaying. Beginning process of shuffling songs in playlist

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -57,20 +57,44 @@ function createReviewElement(review) {
    imgModal.src = review.playlist_image;
 
 
-   
+   const songList = document.getElementById("songList");
+   songList.innerHTML = " ";
 
-   // review.songs.forEach(song => {
-   //    const songTitle = document.getElementById("songTitle");
-   //    songTitle.innerHTML = song.title;
-   //    const songArtist = document.getElementById("songArtist")
-   //    songArtist.innerHTML = "Artist: " + song.artist;
-   //    const songDuration = document.getElementById("songDuration");
-   //    songDuration.innerHTML = song.duration;
-   //    const songImg = document.getElementById('songImg');
-   //    songImg.src = song.img;
-   //    const songAlbum = document.getElementById('songAlbum');
-   //    songAlbum.innerHTML = "Album: " + song.album;
-   // })
+   review.songs.forEach(song => {
+      const songContainer = document.createElement("div");
+      songContainer.classList.add("songLeftSide");
+
+      const img = document.createElement("img");
+      img.src = song.img;
+      img.classList.add("songImg");
+
+      const songInfo = document.createElement("div");
+      songInfo.classList.add("songInfo");
+
+      const title = document.createElement("h2");
+      title.classList.add("songTitle");
+      title.textContent = song.title;
+      songInfo.appendChild(title);
+
+      const artist = document.createElement("p");
+      artist.classList.add("songArtist");
+      artist.textContent = "Artist: " + song.artist;
+      songInfo.appendChild(artist);
+
+      const album = document.createElement("p");
+      album.classList.add("songAlbum");
+      album.textContent = song.album;
+      songInfo.appendChild(album);
+
+      const duration = document.createElement("p");
+      duration.classList.add("songAlbum");
+      duration.textContent = song.duration;
+      songInfo.appendChild(duration);
+
+      songContainer.appendChild(img);
+      songContainer.appendChild(songInfo);
+      songList.appendChild(songContainer);
+   });
 
    
    modal.style.display = "block";
